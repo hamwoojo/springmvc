c44aff347f14937c044d17e51bea9f8c9b300134
hamwoojo <woojo3398@naver.com>
2023-10-16 19:39:33 +0900

    [feat] 요청 파라미터 처리 예제 - requestHeader 종류 및 파라미터 받기 - responseBody 어노테이션 활용 방법 - modelattribute 활용 방법

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,76 @@
+package hello.springmvc.basic.request;
+
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userName = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("userinfo={}, age={}",userName,age);
+
+        response.getWriter().write("ok");
+    }
+
+    @RequestMapping("/request-param-v2")
+    @ResponseBody
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+    * @RequestParam 사용
+    * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능 */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username,int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(),helloData.getAge());
+        log.info("hellodata={}",helloData);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(),helloData.getAge());
+        log.info("hello data={}",helloData);
+        return "ok";
+    }
+}
